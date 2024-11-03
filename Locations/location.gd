cbf6746cb54cb5fde0f3eb0f212a7c06f538b930
Greg Donnell <gregory.donnell74@gmail.com>
2024-11-03 17:39:13 -0600

    First version of location logic

---
+++ Locations/location.gd
@@ -0,0 +1,49 @@
+extends Resource
+class_name Location
+
+enum TimeOfDay {
+	BREAKFAST,
+	MORNING,
+	LUNCH,
+	AFTERNOON,
+	DINNER,
+	EVENING
+}
+
+@export var description : String = "<Unknown>"
+@export var morningActivities : Array[Activity] = []
+@export var afternoonActivities : Array[Activity] = []
+@export var eveningActivities : Array[Activity] = []
+@export var currentTimeOfDay : TimeOfDay = TimeOfDay.BREAKFAST
+
+func simulate() -> void:
+	print("Simulating next part of day: %s" % TimeOfDay.keys()[currentTimeOfDay])
+	var activitiesToSimulate : Array[Activity] = []
+	match currentTimeOfDay:
+		TimeOfDay.MORNING:
+			activitiesToSimulate = morningActivities
+		TimeOfDay.AFTERNOON:
+			activitiesToSimulate = afternoonActivities
+		TimeOfDay.EVENING:
+			activitiesToSimulate = eveningActivities
+		_: #default
+			print("Unknown time of day: %d" % currentTimeOfDay)
+
+	for each_activity in activitiesToSimulate:
+		print("Simulating activity: %s" % each_activity.description)
+		simulate_activity(each_activity)
+
+	advance_time_of_day()
+
+func simulate_activity(activity: Activity) -> void:
+	if activity.rewardItems.size() > 0:
+		for each_reward in activity.rewardItems:
+			PlayerInventoryController.add_item(each_reward)
+		
+		# handle decrementing 
+		PartyController.apply_party_damage(PartyController.Stats.STRENGTH, activity.strengthCost)
+		PartyController.apply_party_damage(PartyController.Stats.STAMINA, activity.staminaCost)
+		PartyController.apply_party_damage(PartyController.Stats.HEALTH, activity.healthCost)
+	
+func advance_time_of_day() -> void:
+	currentTimeOfDay = (currentTimeOfDay + 1) as TimeOfDay
--- Party/party_member.gd
+++ Party/party_member.gd
@@ -5,3 +5,17 @@ class_name PartyMember
 @export var health : float = 10.0
 @export var stamina : float = 10.0
 @export var strength : float = 10.0
+@export var level : int = 1
+
+func decrement_health(amount: float) -> void:
+	health -= amount
+
+func decrement_stamina(amount: float) -> void:
+	stamina -= amount
+
+func decrement_strength(amount: float) -> void:
+	strength -= amount
+
+func _to_string() -> String:
+	return "%s: Level: %d, Health: %d, Stamina: %d, Strength: %d" % \
+			[name, level, health, stamina, strength]
