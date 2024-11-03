45301714863d6b1ff0beff83de40da1730de3a3c
Greg Donnell <gregory.donnell74@gmail.com>
2024-11-02 20:30:17 -0500

    Make mashed potatoes!

--- Inventory/inventory_controller.gd
+++ Inventory/inventory_controller.gd
@@ -13,18 +13,23 @@ func take_item(item_index: int, quantity_needed: int = 1) -> InventoryItem:
 		print("Invalid index of inventory: %d when size is %d" % [item_index, inventory.items.size()])
 		return null
 
-	if inventory.items[item_index].quantity < quantity_needed:
-		print("Not enough quantity of the item to meet quantity_needed: %d vs %d" % [inventory.items[item_index].quantity, quantity_needed])
-		return null
-	elif inventory.items[item_index].quantity == quantity_needed:
-		return inventory.items.pop_at(item_index)
-	else:
-		inventory.items[item_index].quantity -= quantity_needed
-		var item = inventory_item_resource.new()
-		item.name = inventory.items[item_index].name
-		item.quantity = quantity_needed
-		item.texture = inventory.items[item_index].texture
-		return item
+	# TODO: Implement item stacks for quantity
+	#if inventory.items[item_index].quantity < quantity_needed:
+		#print("Not enough quantity of the item to meet quantity_needed: %d vs %d" % [inventory.items[item_index].quantity, quantity_needed])
+		#return null
+	#elif inventory.items[item_index].quantity == quantity_needed:
+	return inventory.items.pop_at(item_index)
+	#else:
+		#inventory.items[item_index].quantity -= quantity_needed
+		#var item = inventory_item_resource.new()
+		#item.name = inventory.items[item_index].name
+		#item.quantity = quantity_needed
+		#item.texture = inventory.items[item_index].texture
+		#item.transistions = inventory.items[item_index].transistions
+		#return item
+
+func clear_inventory() -> void:
+	inventory.items = []
 
 
 func _to_string() -> String:
--- Inventory/inventory_item.gd
+++ Inventory/inventory_item.gd
@@ -2,5 +2,4 @@ extends Resource
 class_name InventoryItem
 
 @export var name : String = "<Unknown>"
-@export var quantity : int = 1
 @export var texture : Texture2D
