cfd558f756c352aae97f979fc6f417f8ee402952
Greg Donnell <gregory.donnell74@gmail.com>
2024-11-11 19:34:41 -0600

    Add icons and resources for v2 cooking stations

--- Cooking/actions.gd
+++ Cooking/actions.gd
@@ -3,13 +3,10 @@ extends Node
 # Always add to end otherwise saves will break
 enum Actions {
 	WAIT   = 0, # No action, no station
-	DICE   = 1, # cutting board
-	PEEL   = 2, # cutting board
-	BOIL   = 3, # cooking pot
-	SAUTE  = 4, # cooking pot
-	SIMMER = 5, # cooking pot
-	MELT   = 6, # cooking pot
-	MASH   = 7, # cooking pot
-	CHOP   = 8, # cutting board
-	MIX    = 9, # cooking pot
+	DICE   = 1, # knife
+	PEEL   = 2, # peeler
+	BOIL   = 3, # Boiling pot
+	SAUTE  = 4, # sauce pan
+	MASH   = 5, # masher
+	MIX    = 6, # Bowl
 }
