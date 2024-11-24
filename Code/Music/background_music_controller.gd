82f516074987639fe2b41a6d1d49f2dd970c9041
Greg Donnell <gregory.donnell74@gmail.com>
2024-11-23 21:25:40 -0600

    Add background music controller and first two albums data

---
+++ Code/Music/background_music_controller.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@export var soundtrack : Array[Song] = []
+@export var currently_playing_index = 0
+
+func _ready() -> void:
+	soundtrack.shuffle()
+	_switch_track(currently_playing_index)
+	%BgMusicPlayer.finished.connect(_on_bg_music_player_finished)
+
+func _switch_track(index: int) -> void:
+	currently_playing_index = index
+	%BgMusicPlayer.stream = soundtrack[currently_playing_index].stream
+	%BgMusicPlayer.play()
+	_update_music_info()
+	%SongCanvasLayer.visible = true
+	%SongVisibleTimer.start()
+
+func _update_music_info() -> void:
+	var song = soundtrack[currently_playing_index]
+	%AlbumTexture.texture = song.album.art
+	%AlbumTexture.texture
+	%SongTitleLabel.text = song.title
+	%AlbumTitleLabel.text = song.album.title
+	%ArtistLabel.text = song.album.artist
+
+func _on_bg_music_player_finished() -> void:
+	_switch_track(currently_playing_index + 1)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Play Last Song"):
+		_switch_track(currently_playing_index - 1)
+
+	if event.is_action_pressed("Play Next Song"):
+		_switch_track(currently_playing_index + 1)
+
+func _on_song_visible_timer_timeout() -> void:
+	%SongCanvasLayer.visible = false
---
+++ Code/Music/song.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Song
+
+@export var stream : AudioStream
+@export var title : String
+@export var album : Album
